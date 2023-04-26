3e425c273d8d31049a7b6f38fb6a632c4aade681
Aleksey Chemakin <Dzenly@users.noreply.github.com>
2023-04-26 15:49:41 +0700

    Update get-git-projects.js

--- pure-http/get-git-projects.js
+++ pure-http/get-git-projects.js
@@ -1,4 +1,4 @@
-// Gets all project HTTPS path to clone from self-hosted gitlab. Here gitlab is hardcoded, so script presented as is.
+// Gets all project HTTPS paths from self-hosted gitlab. Here gitlab is hardcoded, so script presented as is.
 
 const https = require('https');
 
